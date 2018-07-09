b0c9d8e43e50b53d999b2254e7de87cbaad3df42
Ahmadreza.smdi <ahmadreza.smdi@gmail.com>
2018-07-09 23:27:35 +0430

    update on comments

--- doctors.php
+++ doctors.php
@@ -1,10 +1,9 @@
 <?php
     session_start();
     require_once ('functions.php');
-    include_once("css_links.php");
-    include_once("js_links.php");
-    $doctor_id = $_GET["id"];
-    $doctor = getDoctor($doctor_id);
+
+    $doctor_identity = $_GET["id"];
+    $doctor = getDoctor($doctor_identity);
 ?>
 
 <head>
@@ -12,19 +11,19 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 <title>Doctor</title>
 
-<body>
-    <div id='doctor_information'>
-        <h1>Doctor name :</h1>
-        <h4><?php $doctor['name'] ?></h4>
+<body dir='rtl'>
+    <div id='doctor_information' align='center'>
+        <p><b>اسم دکتر:</b></p>
+        <p><?php echo $doctor['name']; ?></p>
+        
+        <p><b>آدرس:</b></p>
+        <p><?php echo $doctor['address'] ?></p>
 
-        <h1>Address :</h1>
-        <h4><?php $doctor['address'] ?></h4>
+        <p><b>شماره تلفن:</b></p>
+        <p><?php echo $doctor['phone'] ?></p>
 
-        <h1>Phone :</h1>
-        <h4><?php $doctor['phone'] ?></h4>
-
-        <h1>Expertise :</h1>
-        <h4><?php $doctor[''] ?></h4>
+        <p><b>تخصص:</b></p>
+        <p><?php echo $doctor['expertise_name'] ?></p>
 
     </div>
     
@@ -42,27 +41,49 @@
             $comment = $_REQUEST['comment'];
             $rate = $_REQUEST['menu'];
     
-            $sql="insert into doctors_comments set patient_id='$patientId', doctor_id = '$doctorId',comment_text = '$comment',comment_score='$menu' " ;
-            $conn->query($sql);
+            $sql="insert into doctors_comments set patient_id='$patientId', doctor_id = '$doctorId',comment_text = '$comment',comment_score='$rate' " ;
+            if($conn->query($sql)) echo 'با موفقیت ثبت شد';
+            else;
         }
         if ($patient==false)die;
     ?>
     
-    <div id='get_comments'>
-        <form action="addcomment.php" method='post' name='comm'>
+<div align='center'>
+    <hr>
+    <?php 
+       $commm = getAllComments();
+       for($i=0; $i<count($commm); $i++){     
+        echo '<p> '.'<b>' .$commm[$i]['name']. '</b>' .' </p>' ;
+        echo '<p> '.$commm[$i]['comment_text']. ' </p>' ;
+        echo '<p> '.'امتیاز:' .$commm[$i]['comment_score']. ' </p>' ;
+        echo '<hr>';
+       }
+    ?>
+</div>
+    <div id='get_comments' align='center'>
+        <form method='post' name='comm'>
             <p> 
-                <label for="comment"> Comment </label>
-                <textarea name="comment" id="" cols="30" rows="10">a</textarea>
+                <br>
+                <br>
+                <p>نظر خود را وارد کنید</p>
+                <textarea name="comment" id="" cols="30" rows="10"></textarea>
             </p>
-
+            <br>
+            <p>به این دکتر چه نمره ای می دهید؟</p>
             <select name="menu">
-            <P> Your point</P>
-            <option value="0" selected></option>
-            <option value="1">one</option>
-            <option value="2">two</option>
-            <option value="3">three</option>
+                <option value="0"></option>
+                <option value="1">۱</option>
+                <option value="2">۲</option>
+                <option value="3">۳</option>
+                <option value="3">۴</option>
+                <option value="3">۵</option>
+                <option value="3">۶</option>
+                <option value="3">۷</option>
+                <option value="3">۸</option>
+                <option value="3">۹</option>
+                <option value="3">۱۰</option>
             </select>
-            <div><input name='sub' type="submit" value="submit"></div>
+            <div><input name='sub' type="submit" value="ثبت نظر"></div>
         </form>
     </div>
 </body>
