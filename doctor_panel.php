1956aadfc46cb72e7428795968f86febd99a3ec4
sadeghb97 <sadeghb97@gmail.com>
2018-07-09 17:42:38 +0430

    show and edit doctors details in doctor panel

--- doctor_panel.php
+++ doctor_panel.php
@@ -1,19 +1,35 @@
 <?php
-    require_once ('functions.php');
-    include_once("css_links.php");
-    include_once("js_links.php");
     session_start();
-    $doctor_id = $_GET["doctor_id"];
-    $conn=db_connect();
-    $query_1 = "select * from doctors inner join expertise on doctors.expertise_id = expertise.id where doctors.id =$doctor_id;";
-    $query_2 = "select * from cliniks_doctors inner join cliniks on cliniks_doctors.clinik_id = cliniks.id where doctor_id =$doctor_id;";
-    $query_3 = "select * from free_times inner join time_slots on free_times.time_slot_id = time_slots.id where doctor_id =$doctor_id;";
-    $query_4 = "select * from reserves  inner join free_times on reserves.free_time_id = free_times.id inner join time_slots on free_times.time_slot_id = time_slots.id where free_times.doctor_id =$doctor_id;";
+    require_once ('functions.php');
+    
+    if(empty($_SESSION['identity'])) header("location:index.php");
+    $identity = $_SESSION['identity'];
+    $doctor = getDoctor($identity);
+    if($doctor===false) header("location:index.php");
+    
+    if(isset($_POST['submit'])){
+        $conn = db_connect();
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $password = $_POST['password'];
+        $newIdentity = $_POST['melli'];
 
-    $res_1=$conn->query($query_1);
-    $res_2=$conn->query($query_2);
-    $res_3=$conn->query($query_3);
-    $res_4=$conn->query($query_4);
+        $sql="update doctors set name='$name', phone='$phone', address='$address', ";
+                if($password!="") $sql .= "password='$password', ";
+                $sql .= "identity='$newIdentity' WHERE identity='$identity'";
+
+        if ($conn->query($sql) === TRUE) {
+            boldEcho("اطلاعات با موفقیت بروز شد!", "green",0,2);
+            $_SESSION['identity']=$newIdentity;
+            $identity=$newIdentity;
+            $doctor = getDoctor($identity);
+            if($doctor===false) header("location:index.php");
+        }
+        else {
+            boldEcho("خطا در بروزرسانی!", "red",0,2);
+        }
+    }
 ?>
 <head>
 <meta charset="UTF-8" />
@@ -24,108 +40,126 @@
     include_once("js_links.php");
 ?>
 <link rel="stylesheet" type="text/css" href="./assets/css/lr.css">
-	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        
+    <script language="javascript">
+        function submitForm(){
+            let melli = document.getElementById('melli').value;
+            let name = document.getElementById('name').value;
+            let phone = document.getElementById('phone').value;
+            let adress = document.getElementById('address').value;
+            let pass = document.getElementById('password').value;
+            let passConf = document.getElementById('password_conf').value;
+
+            if(!melli){alert("کد ملی را وارد کنید!"); return false;}
+            if(!name){alert("نام و نام خوانوادگی را وارد کنید!"); return false;}
+            if(!phone){alert("شماره تماس را وارد کنید!"); return false;}
+            if(!adress){alert("آدرس را وارد کنید!"); return false;}
+
+            if(isNaN(melli)){ alert("کد ملی نامعتبر است!"); return false;}
+            if(isNaN(phone)){ alert("شماره تلفن نامعتبر است"); return false;}
+            if(!(pass === passConf)){ alert("عدم تطابق رمز عبور!"); return false;}
+        }
+    </script>
 </head>
 
-<body>
-<div id="doctor_info">
-    <?php
-        $identity="";
-        $name="";
-        $address="";
-        $phone="";
-        $password="";
-        $expertise_name="";
-        if($res_1->num_rows > 0){
-            $row = $res_1->fetch_assoc();
-            $identity = $row["identity"];
-            $name = $row["name"];
-            $address = $row["address"];
-            $phone = $row["phone"];
-            $password = $row["password"];
-            $expertise_name = $row["expertise_name"];
-        }
-    ?>
-    <label for="identity">کد ملی</label>
-    <input type="text" value=<?php echo $identity; ?> >
-    <br/>
-    <label for="name">نام</label>
-    <input type="text" value=<?php echo $name; ?> >
-    <br/>
-    <label for="address">آدرس</label>
-    <input type="text" value=<?php  echo $address; ?> >
-    <br/>
-    <label for="phone">شماره تلفن</label>
-    <input type="text" value=<?php echo $phone; ?> >
-    <br/>
-    <label for="password">پسورد</label>
-    <input type="password" value=<?php echo $password; ?> >
-    <br/>
-    <label for="expertise_name">تخصص</label>
-    <input type="text" value=<?php echo $expertise_name; ?> >
-    <br/>
-</div>
+<body style="direction: rtl;">
+    <form method="post">
+        <div id="doctor_info">
+            <label for="identity">کد ملی</label>
+            <input type="text" id="melli" name="melli" value="<?php echo $doctor['identity']; ?>">
+            <br/>
+            <label for="name">نام</label>
+            <input type="text" id="name" name="name" value="<?php echo $doctor['name']; ?>">
+            <br/>
+            <label for="address">آدرس</label>
+            <input type="text" id="address" name="address" value="<?php  echo $doctor['address']; ?>">
+            <br/>
+            <label for="phone">شماره تلفن</label>
+            <input type="text" id="phone" name="phone" value="<?php echo $doctor['phone'] ?>">
+            <br/>
+            <label for="password">پسورد</label>
+            <input type="password" id="password" name="password">
+            <br/>
+            <label for="password">پسورد</label>
+            <input type="password" id="password_conf" name="password_conf">
+            <br/>
+        </div>
+        
+        <input type="submit" value="ثبت تغییرات" name="submit" onclick="return submitForm();">
+    </form>
 
-<div id="doctor_clinik_info">
-  
-</div>
-
-<div id="doctor_free_times">
-    <?php
-        echo '<table>';
-            if($res_3->num_rows > 0){
-                echo "<tr>";
-                        echo "<td>شناسه</td>";
-                        echo "<td>تاریخ</td>";
-                        echo "<td>زمان آغاز</td>";
-                        echo "<td>زمان پایان</td>";
-                echo "</tr>";
-                while ($row = $res_3->fetch_assoc()){
-                    $id = $row["id"];
-                    $date = $row["date"];
-                    $start_time = $row["start_time"];
-                    $end_time = $row["end_time"];
-                    echo "<tr>";
-                        echo "<td>$id</td>";
-                        echo "<td>$date</td>";
-                        echo "<td>$start_time</td>";
-                        echo "<td>$end_time</td>";
-                    echo "</tr>";
-                }
+    <div id="doctor_clinik_info">
+        <br>
+        <label for="phone">تخصص: </label>
+        <?php echo $doctor['expertise_name']; ?>
+        <br>
+        <label for="phone">کلینیک: </label>
+        <?php
+            $clinik = getDoctorClinik($doctor['id']);
+            if(($clinik!=false)){
+                echo $clinik['city_name'] . ' - ' . $clinik['address'] . ' - ' . $clinik['clinik_name'] . '<br>';
             }
-        echo '</table>';
-    ?>
-</div>
+        ?>
+        <br>
+    </div>
 
-<div id="doctor_reserved_times">
-    <?php
-        echo '<table>';
-            if($res_4->num_rows > 0){
-                echo "<tr>";
-                        echo "<td>شناسه</td>";
-                        echo "<td>شناسه بیمار</td>";
-                        echo "<td>تاریخ</td>";
-                        echo "<td>زمان آغاز</td>";
-                        echo "<td>زمان پایان</td>";
-                echo "</tr>";
-                while ($row = $res_4->fetch_assoc()){
-                    $id = $row["id"];
-                    $patient_id = $row["patient_id"];
-                    $date = $row["date"];
-                    $start_time = $row["start_time"];
-                    $end_time = $row["end_time"];
+    <div id="doctor_free_times">
+        <?php
+            /*echo '<table>';
+                if($res_3->num_rows > 0){
                     echo "<tr>";
-                        echo "<td>$id</td>";
-                        echo "<td>$date</td>";
-                        echo "<td>$start_time</td>";
-                        echo "<td>$end_time</td>";
+                            echo "<td>شناسه</td>";
+                            echo "<td>تاریخ</td>";
+                            echo "<td>زمان آغاز</td>";
+                            echo "<td>زمان پایان</td>";
                     echo "</tr>";
+                    while ($row = $res_3->fetch_assoc()){
+                        $id = $row["id"];
+                        $date = $row["date"];
+                        $start_time = $row["start_time"];
+                        $end_time = $row["end_time"];
+                        echo "<tr>";
+                            echo "<td>$id</td>";
+                            echo "<td>$date</td>";
+                            echo "<td>$start_time</td>";
+                            echo "<td>$end_time</td>";
+                        echo "</tr>";
+                    }
                 }
-            }
-        echo '</table>';
-    ?>
-</div>
+            echo '</table>';*/
+        ?>
+    </div>
+
+    <div id="doctor_reserved_times">
+        <?php
+            /*echo '<table>';
+                if($res_4->num_rows > 0){
+                    echo "<tr>";
+                            echo "<td>شناسه</td>";
+                            echo "<td>شناسه بیمار</td>";
+                            echo "<td>تاریخ</td>";
+                            echo "<td>زمان آغاز</td>";
+                            echo "<td>زمان پایان</td>";
+                    echo "</tr>";
+                    while ($row = $res_4->fetch_assoc()){
+                        $id = $row["id"];
+                        $patient_id = $row["patient_id"];
+                        $date = $row["date"];
+                        $start_time = $row["start_time"];
+                        $end_time = $row["end_time"];
+                        echo "<tr>";
+                            echo "<td>$id</td>";
+                            echo "<td>$date</td>";
+                            echo "<td>$start_time</td>";
+                            echo "<td>$end_time</td>";
+                        echo "</tr>";
+                    }
+                }
+            echo '</table>';*/
+        ?>
+    </div>
 
 </body>
 
