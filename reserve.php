d5a65b81e7274fe7666b2f30dcfa17112a8265cd
alim_74 <alimortazavimanesh74@gmail.com>
2018-07-09 21:30:55 +0430

    front end for reserve.php

--- reserve.php
+++ reserve.php
@@ -26,7 +26,7 @@
     <meta http-equiv="content-language" content="fa-IR">
     <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
     <title>رزرو نوبت دکتر</title>
-    
+    <link rel="stylesheet" type="text/css" href="assets/css/c_reserve.css"/>
     <style>
         #customers {
             font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
@@ -83,34 +83,48 @@
 </head>
 
 <body style="direction: rtl;">
-    <span style="font-weight: bold; font-size: 20px;">لیست نوبت های موجود</span><br>
-    <div id="doctor_free_times">
-        <form method="post" action="reserve.php">
-            <input id="free_time" name="free_time" type="hidden" value="">
-            <table id="customers" style="width: 70%;">
-                <tr>
-                <th>شماره</th>
-                <th>تاریخ</th>
-                <th>زمان</th>
-                <th>رزرو</th>
-                </tr>
+<div class="container" >
+    <?php
+    include_once("navbar.php")
+    ?>
+</div>
+<div class="reserve_main">
+        <div class="list_text">
+            <span style="font-weight: bold; font-size: 20px; margin-top:50x">لیست نوبت های موجود</span>
+        </div>
+        <div class="doctor_free_times">
+            <form method="post" action="reserve.php">
+                <div class="free_t">
+                    <input id="free_time" name="free_time" type="hidden" value="">
+                </div>
+                <div class="table">
+                    <table id="customers" style="width: 70%;">
+                        <tr>
+                        <th class="t1">     شماره</th>
+                        <th>     تاریخ</th>
+                        <th>     زمان</th>
+                        <th class="t4">رزرو     </th>
+                        </tr>
 
-                <?php
-                    for($i=0; count($freeTimes)>$i; $i++){
-                        $freeTime = $freeTimes[$i];
-                        echo "<tr>";
-                        echo "<td>".($i+1)."</td>";
-                        echo "<td>".$freeTime['date']."</td>";
-                        echo "<td>".$freeTime['time']."</td>";
-                        echo "<td>".'<input type="checkbox" id="cb'.$i.'" value="'.$freeTime['id'].'"'.
-                                ' onchange="checkReserve(this.id);">'."</td>";
-                        echo "</tr>";
-                    }
-                ?>
-            </table>
-            
-            <input type="submit" value="ثبت" name="submit_reserve" onclick="return submitForm();">
-        </form>
+                        <?php
+                            for($i=0; count($freeTimes)>$i; $i++){
+                                $freeTime = $freeTimes[$i];
+                                echo "<tr>";
+                                echo "<td>".($i+1)."</td>";
+                                echo "<td>".$freeTime['date']."</td>";
+                                echo "<td>".$freeTime['time']."</td>";
+                                echo "<td>".'<input type="checkbox" id="cb'.$i.'" value="'.$freeTime['id'].'"'.
+                                        ' onchange="checkReserve(this.id);">'."</td>";
+                                echo "</tr>";
+                            }
+                        ?>
+                    </table>
+                </div>
+                <div class="sabt">
+                    <input type="submit" value="ثبت" name="submit_reserve" onclick="return submitForm();">
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 
