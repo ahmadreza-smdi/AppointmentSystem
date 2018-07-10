16a690b60df9278248f6a3da13f04a7f1166e5a9
alim_74 <alimortazavimanesh74@gmail.com>
2018-07-10 12:57:36 +0430

    front end finished.php

--- best_doctors.php
+++ best_doctors.php
@@ -8,6 +8,7 @@
 ?>
 
 <head>
+    <link rel="stylesheet" href="assets/css/c_best_dr.css">
     <meta charset="UTF-8" />
     <title> پزشکان پر مخاطب </title>
     <?php
@@ -41,50 +42,76 @@
     </style>
 </head>
 <body>
-    <span style="font-weight: bold; font-size: 20px;">جدیدترین پزشکان</span><br>
-    <div id="newest_doctors">
-        <table id="customers" style="width: 70%;">
-            <tr>
-            <th>نام پزشک</th>
-            <th>آدرس</th>
-            <th>شماره تلفن</th>
-            </tr>
-            
-            <?php
-                for($i=0; count($newestDoctors)>$i; $i++){
-                    $newDoctor = $newestDoctors[$i];
-                    echo "<tr>";
-                    echo "<td>".$newDoctor['name']."</td>";
-                    echo "<td>".$newDoctor['address']."</td>";
-                    echo "<td>".$newDoctor['phone']."</td>";
-                    echo "</tr>";
-                }
-            ?>
-        </table>
-    </div>
+<div class="t" align="center">
+        <div class="b_5">
+            <span style="font-weight: bold; font-size: 20px;">جدیدترین پزشکان</span><br>
+        </div>
+        <div class="patient_table">
+            <div id="newest_doctors">
+                <table id="customers" style="width: 70%;">
+                    <tr>
+                    <th>نام پزشک</th>
+                    <th>آدرس</th>
+                    <th>شماره تلفن</th>
+                    </tr>
 
-    <br/>
-    <br/>
+                    <?php
+                        for($i=0; count($newestDoctors)>$i; $i++){
+                            $newDoctor = $newestDoctors[$i];
+                            echo "<tr>";
+                            echo "<td>".$newDoctor['name']."</td>";
+                            echo "<td>".$newDoctor['address']."</td>";
+                            echo "<td>".$newDoctor['phone']."</td>";
+                            echo "</tr>";
+                        }
+                    ?>
+                </table>
+            </div>
+        </div>
+</div>
 
-    <span style="font-weight: bold; font-size: 20px;">محبوب ترین  پزشکان</span><br>
-    <div id="most_popular_doctors">
-        <table id="customers" style="width: 70%;">
-            <tr>
-            <th>نام پزشک</th>
-            <th>آدرس</th>
-            <th>شماره تلفن</th>
-            </tr>
-            
-            <?php
-                for($i=0; count($mostPopularDoctors)>$i; $i++){
-                    $mostPopularDoctor = $mostPopularDoctors[$i];
-                    echo "<tr>";
-                    echo "<td>".$mostPopularDoctor['name']."</td>";
-                    echo "<td>".$mostPopularDoctor['address']."</td>";
-                    echo "<td>".$mostPopularDoctor['phone']."</td>";
-                    echo "</tr>";
-                }
-            ?>
-        </table>
+        <br/>
+        <br/>
+
+<div class="t" align="center">
+
+    <div class="b_5">
+        <span style="font-weight: bold; font-size: 20px;">محبوب ترین  پزشکان</span><br>
     </div>
+    <div class="patient_table">
+        <div id="most_popular_doctors">
+            <table id="customers" style="width: 70%;">
+                <tr>
+                <th>نام پزشک</th>
+                <th>آدرس</th>
+                <th>شماره تلفن</th>
+                </tr>
+
+                <?php
+                    for($i=0; count($mostPopularDoctors)>$i; $i++){
+                        $mostPopularDoctor = $mostPopularDoctors[$i];
+                        echo "<tr>";
+                        echo "<td>".$mostPopularDoctor['name']."</td>";
+                        echo "<td>".$mostPopularDoctor['address']."</td>";
+                        echo "<td>".$mostPopularDoctor['phone']."</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </table>
+        </div>
+    </div>
+</div>
 </body>
+
+.patient_table{
+margin-top: 10px;
+padding-top: 50px;
+height: 400px;
+background-color: darkgray;
+width: 100%;
+}
+
+#customers{
+/*float: right;*/
+margin-right: 15%;
+}
